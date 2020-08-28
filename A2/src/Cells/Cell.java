7f9539d835ee5da45c26113f46dafb1f9a711a98
Scott <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-28 18:40:14 +1200

    Update Cell.java
    Added getPlayer() and canTakePlayer() abstract methods

--- A2/src/Cells/Cell.java
+++ A2/src/Cells/Cell.java
@@ -1,16 +1,21 @@
 package Cells;
 
+import CluedoGame.Player;
+
 public interface Cell {
 
+	// Returns the cell's x value
     public int getXPos();
 
+    // Returns the cell's y value
     public int getYPos();
 
-    public default void putPlayer(Player player){}
+    // Places a player in the cell
+    public void putPlayer(Player player);
 
-    public default Player getPlayer(){
-        return null;
-    }
+    // Returns the player currently occupying the cell
+    public Player getPlayer();
 
-
+    // Returns true if the cell can accept a new player
+    public boolean canTakePlayer();
 }
