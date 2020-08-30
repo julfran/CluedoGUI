e5d24741ed5e3f8c6d262026e1bff10b9ff9be49
Scott <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-31 11:41:05 +1200

    Update RoomCell.java

--- A2/src/Cells/RoomCell.java
+++ A2/src/Cells/RoomCell.java
@@ -2,24 +2,46 @@ package Cells;
 
 import Cards.RoomType;
 import CluedoGame.Player;
+import CluedoGame.Room;
 
 public class RoomCell implements Cell {
 
 	private int xPos;
 	private int yPos;
 	private Player player;
-	private RoomType room;
+	private RoomType roomType;
+	private boolean door = false;
+	private Room room;
 
-	public RoomCell(int x, int y, RoomType r){
+	public RoomCell(int x, int y, Room r){
 		xPos =x;
 		yPos =y;
-		room =r;
+		room = r;
+		roomType =r.getType();
 	}
 
 
+	// Door variant constructor
+    public RoomCell(int x, int y, Room r, boolean b) {
+		xPos =x;
+		yPos =y;
+		room = r;
+		roomType =r.getType();
+		if (b) {
+			door = true;
+		}
+	}
 
-    public RoomType getRoomType(){
-		return room;
+    public boolean isDoor() {
+    	return door;
+    }
+    
+    public Room getRoom() {
+    	return room;
+    }
+
+	public RoomType getRoomType(){
+		return roomType;
 	}
 
 	@Override
@@ -45,61 +67,4 @@ public class RoomCell implements Cell {
 	public boolean canTakePlayer() {
 		return true;
 	}
-
-//	private int x;
-//	private int y;
-//	private Board board;
-//	private Player player = null;
-//	private RoomType type;
-//
-//	public RoomCell(int x, int y, RoomType type) {
-//		this.x = x;
-//		this.y = y;
-//		this.board = board;
-//		this.type = type;
-//	}
-//
-//	public int getX() {
-//		return x;
-//	}
-//
-//	public int getY() {
-//		return y;
-//	}
-//
-//	public RoomType getType() {
-//		return type;
-//	}
-//
-//	public boolean isOccupied() {
-//		if (player != null) {
-//			return true;
-//		} else {
-//			return false;
-//		}
-//	}
-//
-//	public Player getPlayer() {
-//		if (isOccupied()) {
-//			return player;
-//		} else {
-//			return null;
-//		}
-//	}
-//
-//	public boolean placePlayer(Player p) {
-//		if (!isOccupied()) {
-//			player = p;
-//			return true;
-//		} else {
-//			return false;
-//		}
-//	}
-//
-//	public void removePlayer() {
-//		player = null;
-//	}
-
-
-
 }
