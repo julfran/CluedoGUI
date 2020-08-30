465b3abaea155f79e05bbf136f805c6b5fc0335c
Scott <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-30 15:48:16 +1200

    Update GUI.java

--- A2/src/GUI/GUI.java
+++ A2/src/GUI/GUI.java
@@ -11,17 +11,13 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.text.DefaultCaret;
 
-public abstract class GUI extends Canvas {
+public abstract class GUI {
 	
 	private JFrame frame;
 	private JPanel topLeftButtons;
-	private JTextArea bottomLeftTextArea;
 	//private GraphicsPane rightGraphicsPane;
-	private JComponent drawing;
-	
-	private JTextField textField;
-	private JPanel opening;		//for adding of players
-	public ArrayList <Player> players = new ArrayList<Player>()
+	//private JComponent drawing;
+	private JComponent rightGraphicsPane;
 	
 	public GUI() {
 		initialise();
@@ -30,7 +26,7 @@ public abstract class GUI extends Canvas {
 	private void initialise(){
 		
 	//=================================================================================
-    //BUTTONS	  
+    //BUTTON METHODS
 	//=================================================================================
 		
     //---------------------------------------------------------------------------------
@@ -103,7 +99,9 @@ public abstract class GUI extends Canvas {
     });
     //---------------------------------------------------------------------------------
 	  
-    //LAYOUT
+    //=================================================================================
+    //WINDOW LAYOUT
+    //=================================================================================
     	
     //Create grid layout
     topLeftButtons = new JPanel(new GridBagLayout());
@@ -114,8 +112,10 @@ public abstract class GUI extends Canvas {
     //Set border
     Border border = BorderFactory.createEmptyBorder(5,5,5,5);
     topLeftButtons.setBorder(border);
-
+    
+    //---------------------------------------------------------------------------------
     //BUTTONS
+    //---------------------------------------------------------------------------------
     
     //start & end button group
     JPanel globalButtons = new JPanel(new GridBagLayout());
@@ -131,7 +131,7 @@ public abstract class GUI extends Canvas {
 	globalButtons.add(quit,c1);
 	
 	topLeftButtons.add(globalButtons,c1);
-	globalButtons.add(Box.createRigidArea(new Dimension(50, 0)));
+	globalButtons.add(Box.createRigidArea(new Dimension(20, 0)));
 	
 	//move, end turn, suggest, accuse button group
 	JPanel actionButtons = new JPanel(new GridBagLayout());
@@ -156,220 +156,35 @@ public abstract class GUI extends Canvas {
 	c2.gridy = 1;
 	actionButtons.add(accuse,c2);
 	topLeftButtons.add(actionButtons,c2);
-	
-	//TEXT AREA
-	bottomLeftTextArea = new JTextArea(80, 80);
-	bottomLeftTextArea.setLineWrap(true);
-	bottomLeftTextArea.setWrapStyleWord(true); // pretty line wrap.
-	bottomLeftTextArea.setEditable(false);
-	JScrollPane scroll = new JScrollPane(bottomLeftTextArea);
-	bottomLeftTextArea.setPreferredSize(new Dimension(250, 250));
-	// these two lines make the JScrollPane always scroll to the bottom when
-	// text is appended to the JTextArea.
-	DefaultCaret caret = (DefaultCaret) bottomLeftTextArea.getCaret();
-	caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
 
-	/*
-	 * finally, make the outer JFrame and put it all together. this is more
-	 * complicated than it could be, as we put the drawing and text output
-	 * components inside a JSplitPane so they can be resized by the user.
-	 * the JScrollPane and the top bar are then added to the frame.
-	 */
-
-	/*JSplitPane leftSplitPanes = new JSplitPane(JSplitPane.VERTICAL_SPLIT,topLeftButtons,bottomLeftTextArea);
-	leftSplitPanes.setDividerSize(5); // make the selectable area smaller
-	leftSplitPanes.setContinuousLayout(true); // make the panes resize nicely
-	leftSplitPanes.setResizeWeight(1); // always give extra space to drawings
-	// JSplitPanes have a default border that makes an ugly row of pixels at
-	// the top, remove it.
-	leftSplitPanes.setBorder(BorderFactory.createEmptyBorder());
-	leftSplitPanes.setTopComponent(drawing);
-	leftSplitPanes.setBottomComponent(scroll);*/
-	//GRAPHICS PANE
-	//rightGraphicsPane = new GraphicsPane();
-	//rightGraphicsPane.setBackground(new Color(255,255,255));
-	//rightGraphicsPane.setBorder(border);
-	//rightGraphicsPane.setSize(200, 200);
-	//JPanel win = new GridLayout(2,2);
+	//---------------------------------------------------------------------------------
+	//GRAPHICS AREA
+	//---------------------------------------------------------------------------------
 	
-	drawing = new JComponent() {
+	rightGraphicsPane = new JComponent() {
 		protected void paintComponent(Graphics g) {
 			redraw(g);
 		}
 	};
-	drawing.setPreferredSize(new Dimension(500,500));
-	// this prevents a bug where the component won't be
-	// drawn until it is resized.
-	drawing.setVisible(true);
+	rightGraphicsPane.setPreferredSize(new Dimension(500,500));
+	rightGraphicsPane.setVisible(true);
 
-	drawing.addMouseListener(new MouseAdapter() {
-		public void mouseReleased(MouseEvent e) {
-			//onClick(e);
-			redraw();
-		}
-	});
-
-	drawing.addMouseWheelListener(new MouseAdapter() {
-		public void mouseWheelMoved(MouseWheelEvent e) {
-		}
-	});
-	
+	//---------------------------------------------------------------------------------
+	//FRAME
+	//---------------------------------------------------------------------------------
 	
 	//Set up the content pane.
 	frame = new JFrame("Window");
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setLocation(50, 50); //window creation location
-	//frame.setSize(200,200);
-    
-    //frame.add(split,2,0);
-    //window.add(leftWindow,BorderLayout.WEST);
-    //frame.add(leftWindow,BorderLayout.NORTH);
-	JSplitPane LeftSplitPanes = new JSplitPane(JSplitPane.VERTICAL_SPLIT,topLeftButtons,bottomLeftTextArea);
-	//LeftSplitPanes.add(topLeftButtons,BorderLayout.NORTH);
-	//LeftSplitPanes.add(bottomLeftTextArea,BorderLayout.CENTER);
-	JSplitPane mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, LeftSplitPanes, drawing);
-	
-    //frame.add(topLeftButtons,BorderLayout.NORTH);
-    //frame.add(bottomLeftTextArea,BorderLayout.CENTER);
-  	//frame.add(rightGraphicsPane,BorderLayout.SOUTH);
+	frame.setLocation(20, 20); //window creation location
+	JSplitPane LeftSplitPanes = new JSplitPane(JSplitPane.VERTICAL_SPLIT,topLeftButtons, new TextPane()); //Split left pane into buttons and text area
+	JSplitPane mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, LeftSplitPanes, rightGraphicsPane); //split left pane from right graphics area
 	frame.add(mainPane,BorderLayout.NORTH);
     frame.pack();
-    frame.setSize(800, 600);
-    frame.setMinimumSize(new Dimension(500,500));
-    frame.setMaximumSize(new Dimension(500,500));
+    frame.setSize(new Dimension(870,660));
     frame.setVisible(true);
-    
-    //rightGraphicsPane.draw();
-    
 	  
-  }
-	
-	//----------------------------------
-		//Opening Frame- setup panel
-		//Choose Characters and username for each player
-		opening = new JPanel();
-		opening.setBounds(10,80,200,30);
-		frame.getContentPane().add(opening);
-		String[] choices = {"MISSSCARLETT", "COLONELMUSTARD", "MRSWHITE",
-								"MRGREEN", "MRSPEACOCK", "PROFESSORPLUM"};
-		JLabel heading = new JLabel("CLUEDO GAME: ENTER YOUR NAME AND CHARACTER.");
-		opening.add(heading);
-
-		//Selection list of players
-		//miss Scarlett radio button
-		JRadioButton missScarlett = new JRadioButton("MISS SCARLETT");
-		missScarlett.setBounds(70,50, 200, 200);
-		opening.add(missScarlett);
-
-		//Colonel mustard radio button
-		JRadioButton colonelMustard = new JRadioButton("COLONELMUSTARD");
-		colonelMustard.setBounds(70, 70,200, 30);
-		opening.add(colonelMustard);
-
-		//Mrs White radio button
-		JRadioButton mrsWhite = new JRadioButton("MRSWHITE");
-		mrsWhite.setBounds(70, 90, 200, 30);
-		opening.add(mrsWhite);
-
-		//Mr Green radio button
-		JRadioButton mrGreen = new JRadioButton("MRGREEN");
-		mrGreen.setBounds(70, 110, 200, 30);
-		opening.add(mrGreen);
-
-		//mrs Peacock radio button
-		JRadioButton mrsPeacock = new JRadioButton("MRSPEACOCK");
-		mrsPeacock.setBounds(70,130, 200,30);
-		opening.add(mrsPeacock);
-
-		//professor plum radio button
-		JRadioButton professorPlum = new JRadioButton("PROFESSORPLUM");
-		professorPlum.setBounds(70,150, 200, 30);
-		opening.add(professorPlum);
-
-
-		//textfield to enter username
-		textField = new JTextField(20);
-		textField.setBounds(10,50,80,20);
-		opening.add(textField);
-		textField.setText("ENTER USERNAME HERE");
-
-		//press start game button when players finish enter their username
-		JButton startGame = new JButton("Start Game");
-		startGame.setBounds(10,180,280,20);
-		opening.add(startGame);
-		startGame.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				frame.getRootPane().requestFocus();
-				if(startGame.isEnabled()){
-					opening.setVisible(false);
-					//need to repaint/show board
-					//set the players to board game
-					//setup players sequence turns
-				}
-			}
-		}
-		);
-
-		//add player button to create players
-		JButton addPlayer = new JButton("ADD PLAYER");
-		addPlayer.setBounds(10,180,280,20);
-		opening.add(addPlayer);
-		addPlayer.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String username = textField.getText();
-				if(username.equals("")){
-					JOptionPane.showMessageDialog(addPlayer, "NO NAME ENTERED.PLEASE ENTER A NAME:");
-					addPlayer.setEnabled(false);
-					return;
-				}
-				for(Player p: players) {
-					if (p.getPlayerName().equals(textField.getText())) {
-						JOptionPane.showMessageDialog(addPlayer, "USERNAME ALREADY EXIST. " +
-								"PLEASE ENTER ANOTHER NAME");
-						return;
-					}
-				}
-
-				if(missScarlett.isEnabled() && missScarlett.isSelected()){
-					missScarlett.setEnabled(false);
-					players.add(new Player(CharacterType.MISSSCARLETT, username));
-				}
-				if(colonelMustard.isEnabled() && colonelMustard.isSelected()){
-					colonelMustard.setEnabled(false);
-					players.add(new Player(CharacterType.COLONELMUSTARD, username));
-				}
-				if(mrsWhite.isEnabled() && mrsWhite.isSelected()){
-					mrsWhite.setEnabled(false);
-					players.add(new Player(CharacterType.MRSWHITE, username));
-				}
-				if(mrsPeacock.isEnabled() && mrsPeacock.isSelected()){
-					mrsPeacock.setEnabled(false);
-					players.add(new Player(CharacterType.MRSPEACOCK, username));
-				}
-				if(professorPlum.isEnabled() && professorPlum.isSelected()){
-					professorPlum.setEnabled(false);
-					players.add(new Player(CharacterType.PROFESSORPLUM, username));
-				}
-				if(mrGreen.isEnabled() && mrGreen.isSelected()){
-					mrGreen.setEnabled(false);
-					players.add(new Player(CharacterType.MRGREEN, username));
-				}
-
-				if(players.size() >6 || players.size() == 0){
-					startGame.setEnabled(false);
-				}
-				else{
-					startGame.setEnabled(true);
-				}
-				frame.getRootPane().requestFocus();
-			}
-
-		});
-
-
-	}
+    }
 
 	protected abstract void redraw();
 
